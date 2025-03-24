ae5306e7ee691017ecad1a93d1318de64faf06ce
Souvik Bag <souvik.engineer007@gmail.com>
2025-03-24 21:00:56 +0530

    no of provinces

--- Graph/7_NoOfProvinces.cpp
+++ Graph/7_NoOfProvinces.cpp
@@ -1,28 +1,49 @@
-// leetcode 547. Number of Provinces (https://leetcode.com/problems/number-of-provinces/)
+#include <bits/stdc++.h>
+using namespace std;
 
 class Solution {
-    public:
-        void dfs(int n,vector<vector<int>> &isConnected, vector<bool> &visited){
-            visited[n]=true;
-            
-            for(int i=0;i<isConnected[n].size();i++){
-                if(isConnected[n][i]==1 && !visited[i]){
-                    dfs(i,isConnected,visited);
-                }
+public:
+    void dfs(int n, vector<vector<int>> &isConnected, vector<bool> &visited) {
+        visited[n] = true;
+        
+        for (int i = 0; i < isConnected[n].size(); i++) {
+            if (isConnected[n][i] == 1 && !visited[i]) {
+                dfs(i, isConnected, visited);
             }
-            
         }
-    
-        int findCircleNum(vector<vector<int>>& isConnected) {
-           int n = isConnected.size();
-           vector<bool> visited(n,false);
-           int count = 0;
-           for(int i=0;i<n;i++){
-            if(!visited[i]){
-                dfs(i,isConnected,visited);
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<bool> visited(n, false);
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i, isConnected, visited);
                 count++;
             }
-           } 
-           return count;
         }
-    };
+        return count;
+    }
+};
+
+int main() {
+    int n;
+    cout << "Enter the number of cities (nodes): ";
+    cin >> n;
+
+    vector<vector<int>> isConnected(n, vector<int>(n));
+    cout << "Enter the adjacency matrix (0/1):\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> isConnected[i][j];
+        }
+    }
+
+    Solution obj;
+    int provinces = obj.findCircleNum(isConnected);
+    cout << "Number of provinces: " << provinces << endl;
+
+    return 0;
+}
